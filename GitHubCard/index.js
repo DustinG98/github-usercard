6a4a98dd961f84959b272043bd1815aef2a78d03
DustinG98 <thedustingraham@gmail.com>
2019-10-31 14:53:37 -0600

    Finished MVP and one stretch goal

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,7 +1,22 @@
 /* Step 1: using axios, send a GET request to the following URL 
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
+
+
+           
 */
+
+const cards = document.querySelector(".cards");
+
+axios.get("https://api.github.com/users/DustinG98")
+  .then(response => {
+    console.log(response.data);
+    const newData = githubCreator(response.data);
+  })
+  .catch(err => {
+    console.log(`The data isn't being returned!`, err);
+  })
+
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -23,8 +38,39 @@
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
-
+ 
+const followers = [];
 const followersArray = [];
+
+
+axios.get("https://api.github.com/users/DustinG98/followers")
+  .then(response => {
+    console.log(response)
+    response.data.forEach(cv => {
+      followers.push(cv.login);
+    })
+    followers.forEach(cv => {
+      axios.get("https://api.github.com/users/" + cv)
+      .then(response => {
+        const newData = githubCreator(response.data);
+    });
+    });
+  })
+
+
+
+
+// axios.get("https://api.github.com/users/" + username)
+//   .then(response => {
+//     // console.log(response);
+//     // const newData = response.data;
+//     // followersArray.forEach(login => {
+//     //   newData.forEach(login2 => {
+//     //     (login === login2.login ? githubCreator(login2) : null);
+//     //   })
+//     // })
+//     const newData = githubCreator(response.data);
+//   })
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,6 +91,48 @@ const followersArray = [];
 </div>
 
 */
+
+
+
+
+function githubCreator(obj){
+  const card = document.createElement("div");
+  const image = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  const h3Name = document.createElement("h3");
+  const username = document.createElement("p");
+  const location = document.createElement("p");
+  const profile = document.createElement("p");
+  const profileLink = document.createElement("a");
+  const followers = document.createElement("p")
+  const following = document.createElement("p")
+  const bio = document.createElement("p")
+
+  cards.appendChild(card);
+  card.append(image, cardInfo);
+  cardInfo.append(h3Name, username, location, profile, followers, following, bio);
+  
+
+  card.classList.add("card");
+  cardInfo.classList.add("card-info");
+  h3Name.classList.add("name");
+  username.classList.add("username");
+
+
+  h3Name.textContent = obj.name;
+  username.textContent = obj.login;
+  location.textContent = `Location: ${obj.location}`
+  profile.textContent = `Profile:`
+  profileLink.textContent = obj.html_url;
+  image.src = obj.avatar_url;
+  profileLink.setAttribute("href", obj.html_url);
+  followers.textContent = `Followers: ${obj.followers}`;
+  following.textContent = `Following: ${obj.following}`;
+  bio.textContent = obj.bio;
+  profile.append(profileLink);
+
+  return card;
+}
 
 /* List of LS Instructors Github username's: 
   tetondan
